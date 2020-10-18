a5eaea086031859fe7b9e0078773de9ee40b0ee4
shenbingjie <857198073@qq.com>
2020-10-18 08:20:27 +0800

    first

---
+++ precache-manifest.47287d223a01a33fcf11ea3ad8622b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebef67735ae87914f44479a325122090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab11d0c03f70981e0e7",
+    "url": "/static/css/main.4ab7aedd.chunk.css"
+  },
+  {
+    "revision": "9d4b7a3480eaa001027a",
+    "url": "/static/js/2.6e8994a5.chunk.js"
+  },
+  {
+    "revision": "03b80fa8fe4a53d7fcc3e612b3bdb43b",
+    "url": "/static/js/2.6e8994a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab11d0c03f70981e0e7",
+    "url": "/static/js/main.6fd82144.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
